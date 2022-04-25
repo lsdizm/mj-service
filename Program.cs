9c7079096b78e0099289bf1f3acb91abb95b9076
lsdizm <lsdizm@naver.com>
2022-04-25 12:47:26 +0900

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.HttpOverrides;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -24,6 +26,11 @@ if (app.Environment.IsDevelopment())
 */
 
 app.UseHttpsRedirection();
+
+app.UseForwardedHeaders(new ForwardedHeadersOptions
+{
+    ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+});
 
 app.UseAuthorization();
 
