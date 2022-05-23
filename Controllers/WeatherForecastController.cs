dea7b7c4afce86ba4a692c7b68418ed650b3be27
lsdizm <lsdizm@naver.com>
2022-05-23 09:47:20 +0900

    data.or.kr연동참조소스

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -30,3 +30,44 @@ public class WeatherForecastController : ControllerBase
         .ToArray();
     }
 }
+
+
+
+/*
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Xml.Serialization;
+using CHIS.DrugReference.Application.Models;
+
+namespace CHIS.DrugReference.Application.Proxies
+{
+    public class MfdsProxy : IMfdsProxy
+    {
+        public MfdsResponse RetrieveMfds(string startChangeDate, string endChangeDate, int pageNo, int takeCount)
+        {
+            // TO-DO : URL, service key를 db로 관리
+            var url = "http://apis.data.go.kr/1471057/MdcinPrductPrmisnInfoService/getMdcinPrductItem"; // URL
+            var serviceKey = "gKTNtNTmRwLKq8JD1zkpfaggw28u5FJ%2F%2BCZ3PpQxX15sOjBrSoWWMf2oSe3dG%2BJqsIcXim5EW5xlTx1jxGqKgA%3D%3D"; // Service Key"
+
+            var path = url + "?ServiceKey=" + serviceKey + "&pageNo=" + pageNo.ToString() + "&numOfRows=" + takeCount +
+                "&start_change_date=" + startChangeDate + "&end_change_date=" + endChangeDate;
+
+            var request = (HttpWebRequest)WebRequest.Create(path);
+            request.Method = "GET";
+
+            using (var response = request.GetResponse())
+            {
+                StreamReader reader = new StreamReader(response.GetResponseStream());
+                var serializer = new XmlSerializer(typeof(MfdsResponse), new XmlRootAttribute("response"));
+                var result = (MfdsResponse)serializer.Deserialize(reader); //직렬화되어있는 xml파일인 reader를 MfdsResponse로 역직렬.
+                return result;
+            }
+        }
+    }
+}
+
+
+*/
